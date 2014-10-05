ebdb87062f1f4a250508c1786f4a077c8438e7f3
Zhu Mengdan(Daniel) <enterzhu@gmail.com>
2014-10-05 23:10:59 +0800

    init project

---
+++ popup.js
@@ -0,0 +1,83 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Global variable containing the query we'd like to pass to Flickr. In this
+ * case, kittens!
+ *
+ * @type {string}
+ */
+var QUERY = 'kittens';
+
+var tinning = {
+ 
+  /**
+   * Sends an XHR GET request to grab photos of lots and lots of kittens. The
+   * XHR's 'onload' event is hooks up to the 'showPhotos_' method.
+   *
+   * @public
+   */
+  requestKittens: function() {
+    // var req = new XMLHttpRequest();
+    // req.open("GET", this.searchOnFlickr_, true);
+    // req.onload = this.showPhotos_.bind(this);
+    // req.send(null);
+  },
+
+  updateBadge: function (bargeText) {
+    var bargeOption = {};
+    bargeOption.text = bargeText.toString();
+    chrome.browserAction.setBadgeText(bargeOption);
+  },
+
+  collectTabs: function () {
+    var queryOption = {};
+    queryOption.currentWindow = true;
+    chrome.tabs.query(queryOption, this.tinningTabs);
+  },
+
+  tinningTabs: function (tabs) {
+    tinning.updateBadge(tabs.length);
+  },
+
+  /**
+   * Handle the 'onload' event of our kitten XHR request, generated in
+   * 'requestKittens', by generating 'img' elements, and stuffing them into
+   * the document for display.
+   *
+   * @param {ProgressEvent} e The XHR ProgressEvent.
+   * @private
+   */
+  showPhotos_: function (e) {
+    var kittens = e.target.responseXML.querySelectorAll('photo');
+    for (var i = 0; i < kittens.length; i++) {
+      var img = document.createElement('img');
+      img.src = this.constructKittenURL_(kittens[i]);
+      img.setAttribute('alt', kittens[i].getAttribute('title'));
+      document.body.appendChild(img);
+    }
+  },
+
+  /**
+   * Given a photo, construct a URL using the method outlined at
+   * http://www.flickr.com/services/api/misc.urlKittenl
+   *
+   * @param {DOMElement} A kitten.
+   * @return {string} The kitten's URL.
+   * @private
+   */
+  constructKittenURL_: function (photo) {
+    return "http://farm" + photo.getAttribute("farm") +
+        ".static.flickr.com/" + photo.getAttribute("server") +
+        "/" + photo.getAttribute("id") +
+        "_" + photo.getAttribute("secret") +
+        "_s.jpg";
+  }
+};
+
+// Run our kitten generation script as soon as the document's DOM is ready.
+$(document).ready(function(){
+  console.log("ready ");
+  tinning.collectTabs();
+});
