99691a1929f792d5da76b2bccc720fe1dd4d15e0
Zhu Mengdan(Daniel) <enterzhu@gmail.com>
2014-10-06 01:55:48 +0800

    Support store the tabs for current window

--- popup.js
+++ popup.js
@@ -8,10 +8,11 @@
  *
  * @type {string}
  */
-var QUERY = 'kittens';
 
 var tinning = {
- 
+   tinningFolderId: null,
+
+
   /**
    * Sends an XHR GET request to grab photos of lots and lots of kittens. The
    * XHR's 'onload' event is hooks up to the 'showPhotos_' method.
@@ -38,46 +39,65 @@ var tinning = {
   },
 
   tinningTabs: function (tabs) {
-    tinning.updateBadge(tabs.length);
+    // Create the folder to store the tabs for current window
+    var now = new Date();
+    var title = (now.getMonth() + 1) + "/" + now.getDate() + " " + now.getHours() + ":" + now.getMinutes();
+    chrome.bookmarks.create({
+        "parentId": tinning.tinningFolderId,
+        "title": title + " " + tabs[0].title
+      },
+      function(newFolder) {
+        for (var i = 0; i < tabs.length; i++) {
+          var tab = tabs[i];
+          chrome.bookmarks.create({
+            "parentId": newFolder.id,
+            "title": tab.title,
+            "url": tab.url
+          });
+        };
+      }
+    );
   },
 
-  /**
-   * Handle the 'onload' event of our kitten XHR request, generated in
-   * 'requestKittens', by generating 'img' elements, and stuffing them into
-   * the document for display.
-   *
-   * @param {ProgressEvent} e The XHR ProgressEvent.
-   * @private
-   */
-  showPhotos_: function (e) {
-    var kittens = e.target.responseXML.querySelectorAll('photo');
-    for (var i = 0; i < kittens.length; i++) {
-      var img = document.createElement('img');
-      img.src = this.constructKittenURL_(kittens[i]);
-      img.setAttribute('alt', kittens[i].getAttribute('title'));
-      document.body.appendChild(img);
-    }
+  // Sync
+  syncTinningFolder: function () {
+    var bookmarkTreeNodes = chrome.bookmarks.getSubTree("2",
+      function(bookmarkTreeNodes) {
+        var otherBookmarks = bookmarkTreeNodes[0].children;
+        for (var i = 0; i < otherBookmarks.length; i++) {
+          if (otherBookmarks[i].title === "Tinning") {
+            tinning.tinningFolderId = otherBookmarks[i].id;
+            tinning.updateBadge(otherBookmarks[i].children.length);
+          }
+        }
+
+        if (tinning.tinningFolderId === null) {
+          tinning.addTinningFolder();
+        }
+      }
+    )
+  },
+  addTinningFolder: function () {
+    chrome.bookmarks.create(
+      {
+        "title": "Tinning"
+      },
+      function(newFolder) {
+        tinning.tinningFolderId = newFolder.id;
+      }
+    );
   },
 
-  /**
-   * Given a photo, construct a URL using the method outlined at
-   * http://www.flickr.com/services/api/misc.urlKittenl
-   *
-   * @param {DOMElement} A kitten.
-   * @return {string} The kitten's URL.
-   * @private
-   */
-  constructKittenURL_: function (photo) {
-    return "http://farm" + photo.getAttribute("farm") +
-        ".static.flickr.com/" + photo.getAttribute("server") +
-        "/" + photo.getAttribute("id") +
-        "_" + photo.getAttribute("secret") +
-        "_s.jpg";
+  registerEvent: function () {
+    $("#tin-tabs").click(function (e) {
+      tinning.collectTabs();
+    });
   }
 };
 
 // Run our kitten generation script as soon as the document's DOM is ready.
 $(document).ready(function(){
   console.log("ready ");
-  tinning.collectTabs();
+  tinning.registerEvent();
+  tinning.syncTinningFolder();
 });
