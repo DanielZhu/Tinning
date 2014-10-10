32bdd78fa40bfd5797ab39be53b32394236fe8ec
Zhu Mengdan(Daniel) <enterzhu@gmail.com>
2014-10-10 23:32:37 +0800

    Add the global event instead of the background.js
    This can improve the performance better than background.js

---
+++ eventPage.js
@@ -0,0 +1,6 @@
+  // Init the tinning important configurations in this method when the extension installed.
+tinning.syncTinningFolder(false);
+
+// Listen the event of bookmarks in background
+chrome.bookmarks.onRemoved.addListener(tinning.onRemoveBookmarks);
+chrome.bookmarks.onCreated.addListener(tinning.onCreatedBookmarks);
