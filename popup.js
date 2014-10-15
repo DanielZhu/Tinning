6b5715e4be39096b802a5eb7ca9af51a4d3bb2c6
Zhu Mengdan(Daniel) <enterzhu@gmail.com>
2014-10-15 16:24:04 +0800

    Tidy up the popup

--- popup.js
+++ popup.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   console.log("ready ");
-
-  // Register all the event first.
-  tinning.registerEvent();
-
   // Sync the configs
   tinning.syncConfig();
   
   // Init the tinning important configurations in this method.
   tinning.syncTinningFolder(true);
+
+  // Register all the event first.
+  tinning.registerEvent();
+
 });
