0d3d010fdb0e5427e1a2453c2547bbff3698e341
Zhu Mengdan(Daniel) <enterzhu@gmail.com>
2014-10-22 00:40:31 +0800

    Support auto scroll after unTin in global

--- eventPage.js
+++ eventPage.js
@@ -1,5 +1,6 @@
   // Init the tinning important configurations in this method when the extension installed.
 tinning.syncTinningFolder(false);
+tinning.registerScrollToLastPositionListener();
 
 // Listen the event of bookmarks in background
 chrome.bookmarks.onRemoved.addListener(tinning.onRemoveBookmarks);
